07538228c980bd7e9f70cadd3db6fc309d253062
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-06-21 17:48:20 +0300

    dizilerle ilgili çalışmalar yapıldı

---
+++ arraysDemo/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// dizilerle çalışma
+        String ogrenci1 = "Engin";
+        String ogrenci2 = "Derin";
+        String ogrenci3 = "Salih";
+        String ogrenci4 = "Betül";
+
+        System.out.println(ogrenci1);
+        System.out.println(ogrenci2);
+        System.out.println(ogrenci3);
+        System.out.println(ogrenci4);
+
+        System.out.println("-------------------------------");
+
+        String[] ogrenciler = new String[4];
+        ogrenciler[0] = "Engin";
+        ogrenciler[1] = "Derin";
+        ogrenciler[2] = "Salih";
+        ogrenciler[3] = "Betül";
+        //ogrenciler[4] = "Ali";
+
+        for(int i = 0; i < ogrenciler.length; i++) {
+            System.out.println(ogrenciler[i]);
+        }
+
+        System.out.println("-------------------------------");
+
+        for(String ogrenci:ogrenciler) {
+            System.out.println(ogrenci);
+        }
+    }
+}
