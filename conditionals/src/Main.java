1b0b1c468738850e96566a122faddce37c7cfae8
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-06-21 17:31:33 +0300

    koşul şartlarıyla ilgili örnekler yapıldı

---
+++ conditionals/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    int sayi = 24;
+
+        if(sayi<20) {
+            System.out.println("Sayı 20'den Küçüktür.");
+        }else if(sayi==20) {
+            System.out.println("Sayı 20'ye Eşittir.");
+        }
+        else {
+            System.out.println("Sayi 20'den Büyüktür.");
+        }
+    }
+}
