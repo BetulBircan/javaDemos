9b73ae766014a25b88ccc9df6c196106137c8e57
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-06-21 17:46:12 +0300

    döngülerle ilgili çalışmalar yapıldı

---
+++ loopDemo/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// for döngüsü
+        for(int i = 2; i <= 10; i+=2) {
+            System.out.println(i);
+        }
+
+        System.out.println("For Döngüsü Bitti");
+
+        // while döngüsü
+        int i = 20;
+
+        while(i <= 10) {
+            System.out.println(i);
+            i+=2;
+        }
+
+        System.out.println("While Döngüsü Bitti");
+
+        // do-while
+        int j = 100;
+        do {
+            System.out.println(j);
+            j+=2;
+        } while(j < 10);
+        System.out.println("Do-While Döngüsü Bitti");
+
+
+
+    }
+}
