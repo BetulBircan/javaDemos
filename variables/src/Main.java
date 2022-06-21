4ecdbffcf25fe5e87053406300b2d27482c7a7f4
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-06-21 17:28:12 +0300

    Değişken ile ilgili örnekler yapılfı

---
+++ variables/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ogrenciSayisi = 12;
+        String mesaj = "Öğrenci Sayısı: ";
+	    System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Öğrenci Sayım: " + ogrenciSayisi);
+        System.out.println("Öğrenci Sayım: " + ogrenciSayisi);
+        System.out.println("Öğrenci Sayım: " + ogrenciSayisi);
+
+    }
+}
