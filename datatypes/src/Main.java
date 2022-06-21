3147eb1076ec4753517e60edf7feef0c4b47678d
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-06-21 17:30:10 +0300

    veri türleri ile ilgili örnekler yapıldı

---
+++ datatypes/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    //int sayi = 12;
+        //sayi = 131111111;
+
+        //byte sayi = 12;
+        //sayi = 128;
+
+        double sayi = 12.5;
+        sayi = -129;
+
+        char karakter = 'A';
+
+        boolean dogruMu = false;
+
+    }
+}
