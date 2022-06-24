e4d13c193a1d12c98745ec57157b00fff19dc8ba
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-06-25 02:01:53 +0300

    metotlarla ilgili örneklemeler yapıldı

---
+++ methods/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+        sayiBulmaca();
+        sayiBulmaca();
+        sayiBulmaca();
+        sayiBulmaca();
+    }
+
+    public static void sayiBulmaca() {
+        // methods
+        int[] sayilar = new int[]{1, 2, 5, 7, 0, 0};
+        int aranacak = 6;
+        boolean varMi = false;
+
+        for(int sayi : sayilar) {
+            if(sayi == aranacak) {
+                varMi = true;
+                break;
+            }
+        }
+        String mesaj = "";
+        if(varMi) {
+            mesaj = "Sayı Mevcut: " + aranacak;
+            mesajVer(mesaj);
+        }
+        else {
+            mesajVer("Sayı Mevcut Değildir: " + aranacak);
+        }
+    }
+    public static void mesajVer(String mesaj) {
+        System.out.println(mesaj);
+    }
+}
