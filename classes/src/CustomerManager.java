e39a57b7c84185190621e8d0f0d185370bde4c12
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-06-25 17:06:35 +0300

    classlarla ilgili çalışmalar eklendi

---
+++ classes/src/CustomerManager.java
@@ -0,0 +1,14 @@
+public class CustomerManager {
+    //Class names start with upper case and that use PascalCase.
+    public void Add() {
+        System.out.println("Müşteri Eklendi");
+    }
+
+    public void Remove() {
+        System.out.println("Müşteri Silindi");
+    }
+
+    public void Update() {
+        System.out.println("Müşteri Güncellendi");
+    }
+}
