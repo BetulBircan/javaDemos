83722f80810e17cc3b53cee9f28ba151478d5fb0
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-06-28 00:04:57 +0300

    encapsulation ve getter setter tanımlaması örneklemeleri yapıldı

--- classesWithAttributes/src/Main.java
+++ classesWithAttributes/src/Main.java
@@ -3,15 +3,16 @@ public class Main {
     public static void main(String[] args) {
         //Product's attribute
         Product product = new Product();
-        product.id = 1;
-        product.name = "Laptop";
-        product.description = "Huawei MateBook D16";
-        product.price = 9000;
-        product.stockAmount = 3;
+        product.setId(1);
+        product.setName("Laptop");
+        product.setDescription("Huawei MateBook D16");
+        product.setPrice(9000);
+        product.setStockAmount(3);
 
         //Product's Managering
         ProductManager productManager = new ProductManager();
         productManager.Add(product);
+        System.out.println("Ürün Kodu: " + product.getKod());
 
         // System.out.println("Ürün Adı: " + product.name);
     }
--- classesWithAttributes/src/ProductManager.java
+++ classesWithAttributes/src/ProductManager.java
@@ -3,4 +3,8 @@ public class ProductManager {
         //JDBS-I will learn later and do it.
         System.out.println("Ürün Eklendi: " + product.name);
     }
+
+     /*public void Add2(int id, String name, String description, int stockAmount, double price, String renk) {
+
+    }*/
 }
