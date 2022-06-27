89583066413c0b32eaafa5680d9a19db0c281975
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-06-28 00:56:24 +0300

    method overloading ile ilgili örneklemeler yapıldı

---
+++ methodOverloading/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        DortIslem dortIslem = new DortIslem();
+        System.out.println("Toplama: " + dortIslem.topla(2, 3));
+        dortIslem.topla(2, 3, 5);
+    }
+}
