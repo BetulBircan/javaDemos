b52c170e0854fe0defbb40f79357e780d82419a1
BetulBircan <bircan.betul_1996@hotmail.com>
2022-09-17 16:53:18 +0300

    açıklama satırları eklendi

--- recapDemo1/src/Main.java
+++ recapDemo1/src/Main.java
@@ -7,14 +7,18 @@ public class Main {
         int sayi3 = 26;
         int enBuyuk = sayi1;
 
+        //enbüyük değişkeni sayi1 kabul edilir. Eğer sayi2 den küçükse enbuyuk değişkenine sayi2 verilir.
         if(enBuyuk < sayi2) {
             enBuyuk = sayi2;
         }
 
+        //enbüyük değişkeni sayi1 kabul edilir. Eğer sayi2 den küçükse enbuyuk değişkenine sayi3 verilir.
         if(enBuyuk < sayi3) {
             enBuyuk = sayi3;
         }
 
+        /*şu anda da en büyük sayı sayi1 değişkeni olduğu için ve de enBuyuk değikeni başta sayi1 e eşit olduğu için
+        ekrana sayi1 in değeri yazılır.*/
         System.out.println("En Büyük Sayı: " + enBuyuk);
 
     }
