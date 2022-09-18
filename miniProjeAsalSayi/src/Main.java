382dda29e1e9c9beeb0a559399b03d053eef48ce
BetulBircan <bircan.betul_1996@hotmail.com>
2022-09-18 20:40:12 +0300

    eğer 2 den küçük sayı girilirse durumları kontrol edildi

--- miniProjeAsalSayi/src/Main.java
+++ miniProjeAsalSayi/src/Main.java
@@ -4,6 +4,16 @@ public class Main {
         int number = 15;
         boolean isPrime = true; //varsayılan olarak asal sayı olduğunu belirttik.
 
+        if(number == 1) {   //Asal sayılar 2 den başlar.  bu yüzden 1 asal sayı değildir.
+            System.out.println("Sayı Asal Değildir");
+            return; //devamı ile ilgilenme burada bitir demek
+        }
+
+        if(number < 1) {
+            System.out.println("Geçersiz Sayı");
+            return;
+        }
+
         //asal sayılarda en küçük sayı 2 dir bu yüzden döngü 2 den başlar
         for(int i = 2; i < number; i++) {
 
