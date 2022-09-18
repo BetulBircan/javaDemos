f0dc7c5c198b252b77013b2f4c20c49c205a5863
BetulBircan <bircan.betul_1996@hotmail.com>
2022-09-18 20:36:32 +0300

    değişkenler tanımlandı

--- miniProjeAsalSayi/src/Main.java
+++ miniProjeAsalSayi/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        int number = 15;
+        boolean isPrime = true; //varsayılan olarak asal sayı olduğunu belirttik.
     }
 }
