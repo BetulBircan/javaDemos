8cc3e707a55c12cc038365ecb78bb16736934e38
BetulBircan <bircan.betul_1996@hotmail.com>
2022-09-18 20:38:13 +0300

    sayının asal olup olmadığı kontrol edildi

--- miniProjeAsalSayi/src/Main.java
+++ miniProjeAsalSayi/src/Main.java
@@ -3,5 +3,20 @@ public class Main {
     {
         int number = 15;
         boolean isPrime = true; //varsayılan olarak asal sayı olduğunu belirttik.
+
+        //asal sayılarda en küçük sayı 2 dir bu yüzden döngü 2 den başlar
+        for(int i = 2; i < number; i++) {
+
+            if(number % i == 0) {
+                isPrime = false;
+            }
+
+        }
+
+        if(isPrime) {
+            System.out.println("Sayı Asaldır.");
+        } else {
+            System.out.println("Sayı Asal Değildir.");
+        }
     }
 }
