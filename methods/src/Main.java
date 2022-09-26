12f095194ba05b618ba91e2fe2c0c5eb9de77c19
BetulBircan <bircan.betul_1996@hotmail.com>
2022-09-26 17:43:22 +0300

    yorum satırları eklendi

--- methods/src/Main.java
+++ methods/src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
+        //Farklı ekrana yazdık olarak düşünülebilir
         sayiBulmaca();
+
+        //Farklı ekrana yazdık olarak düşünülebilir
         sayiBulmaca();
+
+        //Farklı ekrana yazdık olarak düşünülebilir
         sayiBulmaca();
+
+        //Farklı ekrana yazdık olarak düşünülebilir
         sayiBulmaca();
     }
 
